58097b5004249adbfd5a143a836c4aec1433a857
m.guletckii <m.guletckii@sibproauto.ru>
2023-02-12 13:12:42 +0500

    Add dnstlsgtw Class

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import socket
-import ssl
-import os
-import dns.query
+import dnstlsgtw
 
-
-working_path = os.getcwd()
 
 listen_addr = ''
 listen_port = 1853
@@ -12,20 +7,6 @@ listen_port = 1853
 host_name = '1.1.1.1'
 host_port = 853
 
-context_client = ssl.create_default_context()
+connection_threads = []
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0) as sock:
-    sock.bind((listen_addr, listen_port))
-
-    while True:
-        data = dns.query.receive_udp(sock)
-        with context_client.wrap_socket(socket.socket(socket.AF_INET), server_hostname=host_name) as tls:
-                tls.connect((host_name, host_port))
-                dns.query.send_tcp(tls, data[0])
-
-                data_tls = dns.query.receive_tcp(tls)
-                dns.query.send_udp(sock, data_tls[0], data[2])
-
-                tls.close()
-                sock.close()
+gtw = dnstlsgtw.dnstlsgtw(listen_addr, listen_port, host_name, host_port)
