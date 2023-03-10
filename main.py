9f4dd1f88806ac54940fb3433742be6078cbbd69
m.guletckii <m.guletckii@sibproauto.ru>
2023-03-10 15:17:11 +0500

    ENV support, changed Compose file and Dockerfile

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ from dnstlsgtw import dnstlsgtw
 import os,socket, dns.query
 import threading
 
+#Default configuration
+#/////////////////
+DEF_LIST_HOST = '0.0.0.0'
+DEF_LIST_PORT = '53'
+DEF_DNSTLS_HOST = '1.1.1.1'
+DEF_DNSTLS_PORT = '853'
+#////////////////
 
 try:
     listen_addr = str(os.environ['LIST_HOST'])
-    listen_port = int(os.environ['LIST_PORT'])
+    listen_port = int(os.environ.get['LIST_PORT', DEF_LIST_PORT])
     host_name = str(os.environ['DNSTLS_HOST'])
     host_port = int(os.environ['DNSTLS_PORT'])
 except:
     from argparse import ArgumentParser
-    
+
     args_parser = ArgumentParser()
 
-    args_parser.add_argument('--address','-a', required=False, help='Local server address binding', default='0.0.0.0', type=str)
-    args_parser.add_argument('--port','-p', required=True, help='Local server port', default=1853, type=int)
-    args_parser.add_argument('--dnshost','--dh','-A', required=False, help='DNS over TLS host', default='1.1.1.1', type=str)
-    args_parser.add_argument('--dnsport','--dp','-P', required=False, help='DNS over TLS port', default=853, type=int)
+    args_parser.add_argument('--address','-a', required=False, help='Local server address binding', default=DEF_LIST_HOST, type=str)
+    args_parser.add_argument('--port','-p', required=False, help='Local server port', default=DEF_LIST_PORT, type=int)
+    args_parser.add_argument('--dnshost','--dh','-A', required=False, help='DNS over TLS host', default=DEF_DNSTLS_HOST, type=str)
+    args_parser.add_argument('--dnsport','--dp','-P', required=False, help='DNS over TLS port', default=DEF_DNSTLS_PORT, type=int)
 
     args = args_parser.parse_args()
 
